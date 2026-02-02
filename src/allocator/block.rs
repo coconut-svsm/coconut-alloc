b16aa0f38090c7c49fe20edce549e67a86000955
Nicola Ramacciotti <niko.ramak@gmail.com>
2026-02-02 18:08:27 +0100

    allocator/block: Fix unsafe requirements for rust 2024 edition
    Add missing unsafe block to `as_ref` calls as required by the
    new edition.
    
    Signed-off-by: Nicola Ramacciotti <niko.ramak@gmail.com>

--- src/allocator/block.rs
+++ src/allocator/block.rs
@@ -171,12 +171,11 @@ impl<'a> AllocBlock {
     /// for sub-chunk-size allocations and where the allocation bitmap does not
     /// fit into the chunk descriptor.
     pub unsafe fn chunk_bitmap(&'a self, index: usize) -> &'a AtomicU32 {
-        self.chunks[index]
-            .data
-            .as_ptr()
-            .cast::<AtomicU32>()
-            .as_ref()
-            .unwrap()
+        let ptr = self.chunks[index].data.as_ptr().cast::<AtomicU32>();
+        // SAFETY: If the caller ensures the preconditions, the first bytes of
+        // the chunk are reserved for the bitmap and are correctly aligned,
+        // so the pointer is valid.
+        unsafe { ptr.as_ref().unwrap() }
     }
 
     fn make_compound_page(&mut self, index: usize, order: u32) {
